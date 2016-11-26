fa155a8b14d81147637b34a15f39e8578de6af74
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-11-26 21:42:26 +0100

    Allow not updating files if they already exist.

--- index.js
+++ index.js
@@ -51,42 +51,97 @@ args.forEach(function(it) {
 console.log(`Generating ${projectName} with ${JSON.stringify(projectParameters)}.`)
 
 /**
+ * parseFileName - Parse the file name
+ * @param {string} fileName
+ * @return {Object}
+ */
+function parseFileName(fileName) {
+    const result = {
+        name: null,
+        originalName: fileName,
+        keepExisting: false,
+        hbsTemplate: false
+    };
+
+    let name = fileName;
+
+    if (/\.KEEP$/.test(name)) {
+        result.keepExisting = true
+        name = name.substring(0, name.length - ".KEEP".length)
+    }
+
+    if (/\.hbs/.test(name)) {
+        result.hbsTemplate = true
+        name = name.substring(0, name.length - ".hbs".length)
+    }
+
+    result.name = name;
+
+    return result;
+}
+
+/**
  * Recursively process the handlebars templates for the given project.
  * @param {string} currentPath
  * @param {string} fullFolderPath
  */
 function processFolder(currentPath, fullFolderPath) {
     fs.readdirSync(fullFolderPath).forEach(function(fileName) {
-        let fullLocalPath = path.join(currentPath, fileName);
-        let fullFilePath = path.join(fullFolderPath, fileName);
+        let f = parseFileName(fileName)
 
-        if (fs.statSync(fullFilePath).isDirectory()) {
-            console.log(colors.cyan("Creating folder      : " + fullLocalPath))
-            fs.mkdirSync(fullLocalPath);
+        let fullLocalPath = path.join(currentPath, f.name);
+        let fullFilePath = path.join(fullFolderPath, f.originalName);
+
+        if (isDirectory(fullFilePath)) {
+            if (isDirectory(fullLocalPath)) {
+                console.log(colors.yellow("Already exists folder: " + fullLocalPath));
+            } else {
+                console.log(colors.cyan("Creating folder      : " + fullLocalPath))
+                fs.mkdirSync(fullLocalPath);
+            }
+
             processFolder(fullLocalPath, fullFilePath);
             return;
         }
 
-        if (!/\.hbs$/.test(fileName)) {
+
+        if (f.keepExisting && isFile(fullLocalPath)) {
+            console.log(colors.yellow("Keeping regular file : " + fullLocalPath))
+            return;
+        }
+
+        if (!f.hbsTemplate) {
             console.log(colors.cyan("Copying regular file : " + fullLocalPath))
-            fsExtra.copySync(
-                path.join(fullFolderPath, fileName),
-                path.join(currentPath, fileName)
-            )
+            fsExtra.copySync(fullFilePath, fullLocalPath);
             return;
         }
 
 
         console.log(colors.cyan("Parsing HBS template : " + fullLocalPath))
-        let nonTemplateName = /^(.*)\.hbs$/.exec(fileName)[1]
-        fullLocalPath = path.join(currentPath, nonTemplateName);
-
         let templateContent = fs.readFileSync(fullFilePath, "utf-8")
         let template = handlebars.compile(templateContent)
         let content = template(projectParameters)
 
         fs.writeFileSync(fullLocalPath, content, "utf-8")
     });
+}
+
+/**
+ * isDirectory - Checks if the given file path is a directory.
+ * @param {string} name
+ * @return {boolean}
+ */
+function isDirectory(name) {
+    return fs.statSync(name).isDirectory();
+}
+
+/**
+ * isFile - Checks if the given file path is an existing file
+ * @param {string} name
+ * @return {boolean}
+ */
+function isFile(name) {
+    return fs.statSync(name).isFile();
 }
 
 processFolder(".", path.join(ARS_PROJECTS_FOLDER, projectName))
